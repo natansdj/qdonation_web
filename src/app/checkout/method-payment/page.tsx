542f9ac411703fcb1452515f6004d26f4a2e4b97
Reky Senjaya <rekysenjaya@gmail.com>
2024-06-19 06:07:25 +0700

    choose payment

--- src/app/checkout/method-payment/page.tsx
+++ src/app/checkout/method-payment/page.tsx
@@ -18,6 +18,8 @@ export default function MethodPayment() {
   const choose = useAppSelector((state) => state.payment.choose);
   const router = useRouter()
   const [select, setSelect] = useState<number>(0)
+  const [selectInit, setSelectInit] = useState<number>(0)
+  const [selectObj, setSelectObj] = useState({})
 
   useEffect(() => {
     if (!dataList?.items?.length) {
@@ -36,64 +38,6 @@ export default function MethodPayment() {
           <div className="text-[16px] font-medium text-[#1A1B1E] mb-[5px]">Bagaimana kamu melakukan pembayaran</div>
           <div className="text-[14px] text-[#636770]">Tersedia banyak pilihan mudah untuk melakukan pembayaran</div>
         </div>
-        {/* <div className='bg-white rounded-[8px] p-[15px]'>
-          <div className="text-[16px] font-medium text-[#000] mb-[15px]">QoinCash</div>
-          <div className="flex flex-row justify-between cursor-pointer" onClick={() => setSelect('coin' == select ? '' : 'coin')}>
-            <div className="flex gap-[10px] items-center">
-              <Image className='w-[40px] h-[40px]' src={images.donation} alt='' />
-              <div>
-                <div className="text-[14px] font-medium text-[#1A1B1E]">QoinCash</div>
-                <div className="text-[16px] font-medium text-[#F7B500]">Rp{parsingCurrencyRupiah('2000000')}</div>
-              </div>
-            </div>
-            <Checkbox checked={select == 'coin'} />
-          </div>
-        </div>
-        <div className='bg-white rounded-[8px] p-[15px]'>
-          <div className="text-[16px] font-medium text-[#000]">Kartu Debit</div>
-          <AccordionPayment title={<Image src={images.card} alt="" className="w-[30px]" />} >
-            {['BCA', 'Mandiri', 'BNI'].map(item => <div key={item} onClick={() => setSelect(item == select ? '' : item)} className="border-b border-b-[#DEDEDE] last:border-b-0 py-[10px] flex justify-between">
-              <div className="text-[14px] text-[#1A1B1E]">{item}</div>
-              <Checkbox checked={select == item} />
-            </div>)}
-          </AccordionPayment>
-        </div>
-        <div className='bg-white rounded-[8px] p-[15px]'>
-          <div className="text-[16px] font-medium text-[#000]">Transfer lewat bank</div>
-          <AccordionPayment title="Bank AGI" >
-            {['Bank BCA', 'Bank Mandiri', 'Bank BNI'].map(item => <div key={item} onClick={() => setSelect(item == select ? '' : item)} className="border-b border-b-[#DEDEDE] last:border-b-0 py-[10px] flex justify-between">
-              <div className="text-[14px] text-[#1A1B1E]">{item}</div>
-              <Checkbox checked={select == item} />
-            </div>)}
-          </AccordionPayment>
-          <AccordionPayment title="Bank Permata" >
-            {['Bank BCA', 'Bank Mandiri', 'Bank BNI'].map(item => <div key={item} onClick={() => setSelect(item == select ? '' : item)} className="border-b border-b-[#DEDEDE] last:border-b-0 py-[10px] flex justify-between">
-              <div className="text-[14px] text-[#1A1B1E]">{item}</div>
-              <Checkbox checked={select == item} />
-            </div>)}
-          </AccordionPayment>
-          <AccordionPayment title="Bank BRI" >
-            {['Bank BCA', 'Bank Mandiri', 'Bank BNI'].map(item => <div key={item} onClick={() => setSelect(item == select ? '' : item)} className="border-b border-b-[#DEDEDE] last:border-b-0 py-[10px] flex justify-between">
-              <div className="text-[14px] text-[#1A1B1E]">{item}</div>
-              <Checkbox checked={select == item} />
-            </div>)}
-          </AccordionPayment>
-          <AccordionPayment title="Bank BNI" >
-            {['Bank BCA', 'Bank Mandiri', 'Bank BNI'].map(item => <div key={item} onClick={() => setSelect(item == select ? '' : item)} className="border-b border-b-[#DEDEDE] last:border-b-0 py-[10px] flex justify-between">
-              <div className="text-[14px] text-[#1A1B1E]">{item}</div>
-              <Checkbox checked={select == item} />
-            </div>)}
-          </AccordionPayment>
-        </div>
-        <div className='bg-white rounded-[8px] p-[15px]'>
-          <div className="text-[16px] font-medium text-[#000]">Pembayaran Lainnya</div>
-          <AccordionPayment title="Loyalty Points" >
-            {['Styles', 'MAP Club', 'MyPertamina Points', 'Telkomsel Points'].map(item => <div key={item} onClick={() => setSelect(item == select ? '' : item)} className="border-b border-b-[#DEDEDE] last:border-b-0 py-[10px] flex justify-between">
-              <div className="text-[14px] text-[#1A1B1E]">{item}</div>
-              <Checkbox checked={select == item} />
-            </div>)}
-          </AccordionPayment>
-        </div> */}
         {loadingList ?
           [1, 2, 3].map(item => <div key={item} className='bg-white rounded-[8px] p-[15px]'>
             <div className='animate-pulse flex flex-col gap-[15px]'>
@@ -117,7 +61,8 @@ export default function MethodPayment() {
                 if (is_active) {
                   const value = item.id == select ? 0 : item.id
                   setSelect(value)
-                  dispatch(setPaymentChoose({ ...item, type: items.name, type_code: items.code, method_id: items.id }))
+                  setSelectInit(value)
+                  setSelectObj({ ...item, type: items.name, type_code: items.code, method_id: items.id })
                 }
               }} className="border-b border-b-[#DEDEDE] last:border-b-0 py-[10px]">
                 <div className={`flex justify-between ${is_active ? 'cursor-pointer' : 'cursor-not-allowed'}`}>
@@ -134,9 +79,12 @@ export default function MethodPayment() {
             })}
           </div>)}
       </div>
-      {select && <div className='footer-button'>
+      {(select && select == selectInit) && <div className='footer-button'>
         <div className='p-[15px] bg-white flex justify-between items-center border-[#E5E6EB] border-t'>
-          <div onClick={() => router.back()} className='w-full'><Button title='Pilih' type='warning' /></div>
+          <div onClick={() => {
+            dispatch(setPaymentChoose(selectObj))
+            router.back()
+          }} className='w-full'><Button title='Pilih' type='warning' /></div>
         </div>
       </div>}
     </div>
